fc07e5bc0a115afb904ecc0a37a701779af846d1
hαrsh vαirαgi <harshb7839@gmail.com>
2024-06-20 16:06:47 +0530

    class based react component has been added.

---
+++ src/claus/ClassClaus.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ClassClaus extends React.Component {
+  render() {
+    return (
+      <div>
+        <hr/>
+        <h2>Hi! I'm class claus. 🌼</h2>
+        <p>Simple demonstration of class based react component.</p>
+      </div>
+    )
+  }
+}
+
+export default ClassClaus;
